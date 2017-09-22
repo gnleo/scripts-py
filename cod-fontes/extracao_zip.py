6fe8dc2ebeb55b4541fc7d85e310de2d87d48085
gnleo <jamill_gnleo@yahoo.com>
2017-09-22 15:03:56 -0300

    script

---
+++ cod-fontes/extracao_zip.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+import os
+import sys
+import zipfile
+
+def main(path):
+	if not os.path.exists(path):
+		print("Arquivo {} nao existe".format(path))
+		sys.exit(-1)
+	else:
+		zfile = zipfile.ZipFile(path)
+		zfile.extractall()
+		print("Arquivos extraídos")
+
+if __name__ == '__main__':
+	main(sys.argv[1])
